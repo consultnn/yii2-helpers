e58d1407a2bd2d983939dc98d458c2df59db0e33
Maxim MIronyuk <serieznyi@gmail.com>
2016-03-01 14:00:18 +0300

    add ArrayHelper

---
+++ ArrayHelper.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace consultnn\helpers;
+
+use Closure;
+
+class ArrayHelper extends \yii\helpers\ArrayHelper
+{
+    /**
+     * Recursive array_filter
+     *
+     * @param array $input
+     * @param Closure $closure
+     * @return array
+     */
+    public static function arrayFilterRecursive($input, Closure $closure)
+    {
+        foreach ($input as &$value)
+        {
+            if (is_array($value))
+            {
+                $value = self::arrayFilterRecursive($value, $closure);
+            }
+        }
+
+        return array_filter($input, $closure);
+    }
+}
